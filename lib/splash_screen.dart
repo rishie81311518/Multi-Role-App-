552a4ff74ca4e10e56bf140341b7de8295a58ed5
Rishi <rishiekandala@gmail.com>
2024-02-14 00:21:38 +0530

    build splash screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image(
+          height: double.infinity,
+          fit: BoxFit.fitHeight,
+          image: NetworkImage('https://images.pexels.com/photos/1400172/pexels-photo-1400172.jpeg?auto=compress&cs=tinysrgb&w=800')),
+    );
+  }
+}
